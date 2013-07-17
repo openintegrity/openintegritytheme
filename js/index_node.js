52164ae13bebdc2272c5f5452988a6374915f979
Nicolas Borda <nicolas.borda@precedent.co.uk>
2013-07-17 03:54:14 +0100

    Updating jquery to have accordion like functionality

--- js/index_node.js
+++ js/index_node.js
@@ -1,7 +1,19 @@
 jQuery(document).ready(function () {
-  	jQuery(".view-entry .group-level-1 .view-grouping-content").hide();
-    jQuery(".view-entry .group-level-1 .view-grouping-header").show();
-  jQuery(".view-entry .group-level-1 .view-grouping-header").click(function(){
-    jQuery(".view-entry .group-level-1 .view-grouping-content").slideToggle();
-  });
+
+//  	jQuery(".view-entry .group-level-1 .view-grouping-content").hide();
+//    jQuery(".view-entry .group-level-1 .view-grouping-header").show();
+//  jQuery(".view-entry .group-level-1 .view-grouping-header").click(function(){
+//    jQuery(".view-entry .group-level-1 .view-grouping-content").slideToggle();
+//  });
+
+
+    $('.view-entry .group-level-1 .view-grouping-content').hide();
+
+    $('.view-entry .group-level-1 .view-grouping-header').click(function() {
+        $(this).next('.view-entry .group-level-1 .view-grouping-content:hidden').slideDown('fast').siblings('div:visible').slideUp('fast');
+    });
+
+
+
+
 });
