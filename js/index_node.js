f52f2e1c49e61d89367c888de62c009a677c7be5
Nicolas Borda <nicolas.borda@precedent.co.uk>
2013-07-17 03:59:20 +0100

    Fixing small jquery bug in toggle function

--- js/index_node.js
+++ js/index_node.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function () {
     jQuery('.view-entry .group-level-1 .view-grouping-content').hide();
 
     jQuery('.view-entry .group-level-1 .view-grouping-header').click(function() {
-        jQuery(this).next('.view-entry .group-level-1 .view-grouping-content:hidden').slideDown('fast').siblings('div:visible').slideUp('fast');
+        jQuery(this).next('.view-entry div:hidden').slideDown('fast').siblings('div:visible').slideUp('fast');
     });
 
 
