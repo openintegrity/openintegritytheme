8a2a813345c090b33f889cbb124d5b57633955c0
Nicolas Borda <nicolas.borda@precedent.co.uk>
2013-07-18 11:50:49 +0100

    Trying to fix second level accordion

--- js/index_node.js
+++ js/index_node.js
@@ -6,13 +6,10 @@ jQuery(document).ready(function () {
 //    jQuery(".view-entry .group-level-1 .view-grouping-content").slideToggle();
 //  });
 
-
  //   jQuery('.group-level-1 .view-grouping-content').hide();
-
  //   jQuery('.group-level-1 .view-grouping-header').click(function() {
  //       jQuery(this).next('div:hidden').slideDown('fast').siblings('div:visible').slideUp('fast');
  //   });
-
 
 (function($) {
     
@@ -45,7 +42,6 @@ jQuery(document).ready(function () {
   });
 
 })(jQuery);
-
 
 
 });
