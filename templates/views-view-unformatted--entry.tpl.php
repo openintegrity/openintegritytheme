341d6470a2b8032171590be0fe69f9f31907535d
Jun Matsushita <jun@iilab.org>
2013-08-07 17:07:32 +0000

    Move the formatting in the view

--- templates/views-view-unformatted--entry.tpl.php
+++ templates/views-view-unformatted--entry.tpl.php
@@ -8,7 +8,7 @@
  */
 ?>
 <?php if (!empty($title)): ?>
-    <h3><span class="container"><span class="index-trigger">+</span><?php print $title; ?></span></h3>
+    <?php print $title; ?>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
   <div <?php if ($classes_array[$id]) { print 'class="' . $classes_array[$id] .'"';  } ?>>
