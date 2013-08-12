0ea0ef0a77e3a5e551f51d44cd656710d6995ece
Nicolas Borda <nicolas.borda@precedent.co.uk>
2013-08-12 04:56:52 +0100

    Adding initial markup for alternatives carousel

--- templates/node--entry--teaser.tpl.php
+++ templates/node--entry--teaser.tpl.php
@@ -1,10 +1,13 @@
-<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div class="carousel-slide">
   <?php
     // Hide comments, tags, and links now so that we can render them later.
     hide($content['comments']);
     hide($content['links']);
+    hide($content['field_logo']);
   ?>
 
-  <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php print render($content['field_logo']); ?>
+  <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php print render($content); ?>
-</article> <!-- /.node -->
+  <a href="<?php print $node_url; ?>" class="entry-teaser-link">View Report &#62;</a>
+</div> <!-- /.node -->
