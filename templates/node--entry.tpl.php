ce4546625c4c36050d5168714c97a5fc57102336
Nicolas Borda <nicolas.borda@precedent.co.uk>
2013-08-14 04:36:43 +0100

    Adjusting new fields layout on entries

--- templates/node--entry.tpl.php
+++ templates/node--entry.tpl.php
@@ -32,13 +32,15 @@
 
   <div class="row row-entry">
     <div class="container entry-content">
-      <div class="entry-meta">
+      <div class="entry-meta span5 clearfix">
         <div class="entry-info clearfix">
           <div class="entry-info-top clearfix">
             <?php print render($content['field_entry_type']); ?>
             <?php print render($content['field_entry_developer']); ?>
 	      </div>
-          <?php print render($content['field_dependencies']); ?>
+          <div class="entry-info-middle clearfix">
+            <?php print render($content['field_dependencies']); ?>
+	      </div>
           <div class="entry-info-bottom clearfix">
             <?php print render($content['field_entry_category']); ?>
             <?php print render($content['field_entry_version']); ?>
@@ -46,10 +48,9 @@
 	    </div>
         <?php print render($content['field_entry_score']); ?>
 	  </div>
-      <div class="entry-title clearfix">
+      <div class="entry-title span6 clearfix">
         <?php print render($content['field_logo']); ?>
         <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-        <?php print render($content['body']); ?>
         <?php print $node->body[$node->language][0]['safe_value']; ?>
 	  </div>
     </div>
