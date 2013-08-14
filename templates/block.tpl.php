ef8efc5b85d13e727f07b004a1ad7b00bb4f0afb
Jun Matsushita <jun@iilab.org>
2013-08-14 10:34:41 +0000

    Corrected the block.tpl.php

--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -1,12 +1,11 @@
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
-  <?php print (isset($block->title_link))?"<a href='". "/".$block->title_link.'>":""; ?>  
   <?php if ($title): ?>
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif;?>
   <?php print render($title_suffix); ?>
-  <?php print (isset($block->title_link))?"<a href='".$block->title_link."'><h2>":""; ?>  
+  <?php print (isset($block->title_link))?"<a href='/".$block->title_link."'><h2>":""; ?>  
     <?php print $content ?>
   <?php print (isset($block->title_link))?"</h2></a>":""; ?>
 </section>
