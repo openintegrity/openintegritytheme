9bcf2a26b5b7ce658527096fe16d6bc4ed99b2f2
Jun Matsushita <jun@iilab.org>
2013-08-22 13:55:47 +0000

    Implemented Anchro behavior for deep linking inside accordion and click to close

--- js/index_node.js
+++ js/index_node.js
@@ -4,17 +4,20 @@ jQuery(document).ready(function () {
     
   var allPanels = $('.group-level-1 .view-grouping-content').hide();
   var nestedPanels = $('.group-level-1 .view-grouping-content > .views-row').hide();
-    
+  
   $('.group-level-1 .view-grouping-header > h2').click(function() {
       $this = $(this);
       $target =  $this.parent().next();
 
       if(!$target.hasClass('active')){
          allPanels.removeClass('active').slideUp();
-         $target.addClass('active').slideDown();
+         $target.addClass('active').slideDown(function() {
+           $('body').animate({scrollTop:$target.offset().top-100},200);
+         });
+      } else {
+        allPanels.removeClass('active').slideUp();
+        $target.removeClass('active').slideUp();
       }
-      
-    return false;
   });
     
   $('.group-level-1 .view-grouping-content > h3').click(function() {
@@ -23,11 +26,20 @@ jQuery(document).ready(function () {
 
       if(!$target.hasClass('active')){
          nestedPanels.removeClass('active').slideUp();
-         $target.addClass('active').slideDown();
+         $target.addClass('active').slideDown(function() {
+           $('body').animate({scrollTop:$target.offset().top-100},200);
+         });
+         window.location.hash = $this.attr('id');
+      } else {
+        nestedPanels.removeClass('active').slideUp();
+        $target.removeClass('active').slideUp();
       }
-      
-    return false;
   });
+
+  if ( window.location.hash ) {
+      $(window.location.hash).click().parent().parent().children('.view-grouping-header').children('h2').click();
+  }
+    
 
 })(jQuery);
 
